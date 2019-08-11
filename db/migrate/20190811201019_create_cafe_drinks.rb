c98e55edaf460164a1372b0ea1c2f443bfd37946
George Ruiz <georuiz817@gmail.com>
2019-08-11 21:01:58 +0000

    working in app controller

--- db/migrate/20190811201019_create_cafe_drinks.rb
+++ db/migrate/20190811201019_create_cafe_drinks.rb
@@ -1,4 +1,8 @@
-class CreateCafeDrinks < ActiveRecord::Migration[5.2]
+class CreateCafeDrinks < ActiveRecord::Migration
   def change
+    create_table :cafe_drinks do |t|
+      t.integer :drinks_id
+      t.integer :cafe_id 
+    end 
   end
 end
