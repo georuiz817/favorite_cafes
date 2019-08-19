563992b7eded3750c2e89cb85d5f8099d0e1bea1
George Ruiz <georuiz817@gmail.com>
2019-08-19 06:44:41 +0000

    login and signup edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,41 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :home
   end
-end
+  
+  #able to sign up requirement start 
+    get '/signup' do
+        erb :'/users/signup'
+    end
+
+    post '/signup' do
+        user = User.create(params)
+        if user.save
+            session[:user_id] = user.id
+            redirect '/' 
+        else
+           erb :'users/signup'
+      end
+    end
+  #able to sign up requirement end 
+  
+  #able to login requirement start 
+  
+    get '/login' do
+        erb :'/users/login'
+    end
+
+   post '/login' do
+        user = User.find_by(email: params[:email])
+        
+        if user.authenticate(params[:password])
+            session[:user_id] = user.id 
+            redirect '/' 
+        else
+           erb :'users/signup'
+        end
+    end
+    #able to login requirement end 
+  
+  
+  
+  end
